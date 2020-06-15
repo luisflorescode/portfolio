7c0365fb275521a7d5dea1f98b43339c02127f86
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-06-15 16:17:36 -0500

    Initial environment

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>Luis Flores | Frontend Developer</title>
+        <link rel="icon" href="/static/favicon.ico" />
+      </Head>
+    </>
+  );
+}
+
+export default Home;
