ecf8e974ba736269c656c86dba64f3b86f5d8994
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-06-15 17:46:59 -0500

    Header component added

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Logo from '../assets/icons/logo.svg';
+import '../assets/styles/components/Header.scss';
+
+function Header() {
+  return (
+    <header>
+      <div className="header">
+        <Logo />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/_document.jsx
@@ -0,0 +1,34 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="es">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <title>Luis Flores | Frontend Developer</title>
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <meta
+            name="description"
+            content="Luis Flores Frontend Developer Page"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
